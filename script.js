2a053b8fb93c25dfe60a5844b3dbdf668debd96b
Milton Melara <milton.melara3b@gmail.com>
2021-05-30 11:49:10 -0600

    Created compliment generator for make

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ const complimentsForBe = () => {
     const compliments = ['great.', 'someone different.', 'the exception for better.'];
     return compliments[Math.floor(Math.random()* 3)];
 };
+const complimentsForMake = () => {
+    const compliments = ['big things.', 'the world a better place.', 'great changes.'];
+    return compliments[Math.floor(Math.random()* 3)];
+};
