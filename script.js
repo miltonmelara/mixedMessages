e3db49f9ef5ba07261f5bba685ceb56f73a2c736
Milton Melara <milton.melara3b@gmail.com>
2021-05-30 11:43:17 -0600

    Created generator for compliments for be

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const rand2ndWord = () =>{
     return words[Math.floor(Math.random()* 3)];
 };
 const rand3rdWordNotAre = () => {
-    const words = ['be', 'make', 'create',]
+    const words = ['be', 'make', 'create',];
     return words[Math.floor(Math.random()* 3)];
 };
 const rand3rdWordForAre = () => {
-    const words = ['being', 'making', 'creating']
+    const words = ['being', 'making', 'creating'];
     return words[Math.floor(Math.random()* 3)];
+};
+const complimentsForBe = () => {
+    const compliments = ['great.', 'someone different.', 'the exception for better.'];
+    return compliments[Math.floor(Math.random()* 3)];
 };
