98422b1eeaf550924d8b9638f55af934312665c0
Milton Melara <milton.melara3b@gmail.com>
2021-05-30 11:35:15 -0600

    Created the 3rd word generator for words that aren't are

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const rand2ndWord = () =>{
     const words = ['are', 'are going to', 'will'];
     return words[Math.floor(Math.random()* 3)];
-}
+};
+const rand3rdWordNotAre = () => {
+    const words = ['be', 'make', 'create',]
+    return words[Math.floor(Math.random()* 3)];
+};
